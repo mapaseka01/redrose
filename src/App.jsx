df55a1c7392fc2ed8bc6e38643f3866e508bb3cd
unknown <mapasekasemela1@gmail.com>
2023-02-21 12:44:13 +0200

    ...

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar";
+import Home from "./page/Home";
+function App() {
+  return (
+    <div className="app">
+      <Navbar/>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/page/Home.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function Home() {
+  return (
+
+    <div className='bunner'>
+      <div className="card">
+        <img src="https://images.unsplash.com/photo-1547617774-2eb320ad93f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cm9zZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" className="card-img-top" alt="..." />
+        <div className="card-body">
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+
+      <div className="card">
+        <img src="https://images.unsplash.com/photo-1581264692636-3cf6f29655c2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" className="card-img-top" alt="..." />
+        <div className="card-body">
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+      
+      <div className="card">
+        <img src="https://images.unsplash.com/photo-1581264692636-3cf6f29655c2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" className="card-img-top" alt="..." />
+        <div className="card-body">
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
